26150c32d99fe494ecceba590fc6ded190b79671
Emilio Navarro <sainfuw@outlook.com>
2024-09-05 00:29:27 -0400

    fin section 16

--- src/components/Cart/CardCounter.tsx
+++ src/components/Cart/CardCounter.tsx
@@ -23,11 +23,11 @@ export default function CardCounter() {
         width='28'
         height='28'
         viewBox='0 0 24 24'
-        stroke-width='1.5'
+        strokeWidth='1.5'
         stroke='currentColor'
         fill='none'
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeLinecap='round'
+        strokeLinejoin='round'
       >
         <path stroke='none' d='M0 0h24v24H0z' fill='none' />
         <path d='M17.5 17.5m-2.5 0a2.5 2.5 0 1 0 5 0a2.5 2.5 0 1 0 -5 0' />
